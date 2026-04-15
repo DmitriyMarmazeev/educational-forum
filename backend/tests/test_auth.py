7efcf6b4713dfbc223e805c436867856d187f5b3
roman23323 <ignatenko_roman.s@mail.ru>
2026-04-15 20:31:07 +0300

    test(feat): implement all /auth tests

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -22,3 +22,29 @@ async def test_register_validation_weak_password(client, register_data):
     # исправить тест, если это не так
     assert response.status_code != 201, 'Нельзя создавать аккаунт с паролем меньше 6 символов'
     assert response.status_code == 422, 'Должен возвращаться код 422 (Unprocessable Content)'
+
+async def test_register_same_email(client, register_data):
+    first_response = await client.post('/auth/register', json=register_data)
+
+    assert first_response.status_code == 200
+
+    second_response = await client.post('/auth/register', json=register_data)
+
+    assert second_response.status_code != 201, 'Нельзя создавать два аккаунта на один email'
+    assert second_response.status_code == 409, 'Должен возвращаться код 409 (Conflict)'
+
+
+async def test_login_valid(client, register_data):
+    response_register = await client.post('/auth/register', json=register_data)
+    response_login = await client.post('/auth/login', json={
+        'email': register_data['email'],
+        'password': register_data['password']
+    })
+    
+    assert response_login.status_code == 200
+    assert 'application/json' in response_login.headers['content-type'], 'Ответ должен быть формата json'
+
+    data = response_login.json()
+    
+    assert 'access_token' in data, 'В ответе должен быть access_token'
+    assert 'token_type' in data, 'В ответе должен быть указан тип токена token_type'
