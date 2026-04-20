32e6ef0bcd47f847701b26c5c21ad09fc7496688
roman23323 <ignatenko_roman.s@mail.ru>
2026-04-20 18:25:34 +0300

    chore: change .js to .ts and set baseUrl

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -1,11 +1,12 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
+export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:3000/',
   },
 });
